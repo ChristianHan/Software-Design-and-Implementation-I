ad2b7e7230e708aefb05bf0e6cd1550a7a9c7b5f
ee312_cjh3752 <christianhan@utexas.edu>
2016-10-19 17:44:28 -0500

    upload a folder

---
+++ assignment4/assign4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BEGIN_FCN  { 
+#define END_FCN      }
+
+
+
+void main (void){ }
+
+void readSource(){ 
+
+	FILE *fp;
+	fp = fopen(, "r");//put file name before comma
+	
+	
+	
+
+
+
+detectFunction;
+int countLOC;
+printTable;
+
+
